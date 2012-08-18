848279513983535d71c94984e59a6428abcf7a71
Anatoly Pashin <anatoly.pashin@gmail.com>
2012-08-18 16:15:53 +1100

    auto states switching (fixes #25)

---
+++ lib/task/magicStatesTask.class.php
@@ -0,0 +1,48 @@
+<?php
+
+class magicStatesTask extends sfBaseTask
+{
+  protected function configure()
+  {
+    $this->addOptions(array(
+      new sfCommandOption('application', null, sfCommandOption::PARAMETER_REQUIRED, 'The application name'),
+      new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+      new sfCommandOption('connection', null, sfCommandOption::PARAMETER_REQUIRED, 'The connection name', 'doctrine'),
+    ));
+
+    $this->namespace        = 'magic';
+    $this->name             = 'states';
+    $this->briefDescription = 'Switches orders states from/to archived and debt';
+    $this->detailedDescription = <<<EOF
+The [magic:states|INFO] task does things.
+Call it with:
+
+  [php symfony magic:states|INFO]
+EOF;
+  }
+
+  protected function execute($arguments = array(), $options = array())
+  {
+    // initialize the database connection
+    $databaseManager = new sfDatabaseManager($this->configuration);
+    $connection = $databaseManager->getDatabase($options['connection'])->getConnection();
+
+    $archived = Doctrine_Query::create()
+      ->update('Order')
+      ->set('state', '?', 'archived')
+      ->andWhereIn('state', array('submited', 'debt'))
+      ->andWhere('payed >= cost')
+      ->execute()
+    ;
+
+    $debted = Doctrine_Query::create()
+      ->update('Order')
+      ->set('state', '?', 'debt')
+      ->andWhereIn('state', array('submited', 'archived'))
+      ->andWhere('payed < cost or payed is null')
+      ->execute()
+    ;
+
+    echo print_r( array('archived' => $archived, 'debted' => $debted), 1 );
+  }
+}
