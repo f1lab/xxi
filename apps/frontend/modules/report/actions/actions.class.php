c2f5fbde9ccbc3bb09e26afa5fcb738e25c442e2
Anatoly Pashin <anatoly.pashin@gmail.com>
2012-08-25 15:34:12 +1100

    report costs-active

--- apps/frontend/modules/report/actions/actions.class.php
+++ apps/frontend/modules/report/actions/actions.class.php
@@ -19,7 +19,6 @@ class reportActions extends sfActions
   {
     $this->states = array(
       'archived' => 'Архив',
-      'active' => 'Текущие',
       'debt' => 'Дебиторка',
     );
 
@@ -100,25 +99,79 @@ class reportActions extends sfActions
         sum(a.payed) payed
       ')
       ->from('Order a')
+      ->andWhere('a.state = ?', $this->state)
+      ->andWhere('a.submited_at >= ? and a.submited_at <= ?', array($this->period['from'], $this->period['to']))
     ;
 
-    if ($this->state == 'active') {
-      $query
-        ->andWhereIn('a.state', array(
-          'work',
-          'working',
-          'done',
-          //'submited',
-        ))
-      ;
-    } else {
-      $query
-        ->andWhere('a.state = ?', $this->state)
-      ;
+    if ($this->manager) {
+      $query->andWhere('a.created_by = ?', $this->manager);
     }
 
-    $query
-      ->andWhere('a.submited_at >= ? and a.submited_at <= ?', array($this->period['from'], $this->period['to']))
+    $this->report = $query
+      ->execute()
+      ->getFirst()
+    ;
+  }
+
+  public function executeCostsActive(sfWebRequest $request)
+  {
+    $this->form = new sfForm();
+    $this->form->getWidgetSchema()
+      ->offsetSet('manager', new sfWidgetFormDoctrineChoice(array(
+        'model' => 'sfGuardUser',
+        'table_method' => 'getManagers',
+        'add_empty' => 'Все',
+        'label' => 'Менеджер',
+      )))
+      ->setNameFormat('filter[%s]')
+    ;
+    $this->form->addCSRFProtection('123456789');
+    $this->form->getValidatorSchema()
+      ->offsetSet('manager', new sfValidatorDoctrineChoice(array(
+        'model' => 'sfGuardUser',
+        'required' => false,
+      )))
+    ;
+
+    $this->period = array(
+      'from' => date('Y') . '-01-01',
+      'to' => date('Y-m-d'),
+    );
+    $this->manager = false;
+    $this->state = 'archived';
+
+    if ($request->isMethod('post')) {
+      $this->form->bind($request->getParameter('filter'));
+
+      if ($this->form->isValid()) {
+        if ($this->form->getValue('manager')) {
+          $this->manager = $this->form->getValue('manager');
+        }
+
+        if ($this->form->getValue('state')) {
+          $this->state = $this->form->getValue('state');
+        }
+      }
+    }
+
+    $query = Doctrine_Query::create()
+      ->select('
+        count(*) count,
+        sum(a.installation_cost) installation_cost,
+        sum(a.design_cost) design_cost,
+        sum(a.contractors_cost) contractors_cost,
+        sum(a.delivery_cost) delivery_cost,
+        sum(a.cost) cost,
+        sum(a.recoil) recoil,
+        sum(a.payed) payed
+      ')
+      ->from('Order a')
+      ->andWhereIn('a.state', array(
+        'work',
+        'working',
+        'done',
+        //'submited',
+      ))
     ;
 
     if ($this->manager) {
--- apps/frontend/modules/report/templates/_tabs.php
+++ apps/frontend/modules/report/templates/_tabs.php
@@ -1,6 +1,7 @@
 <?php
   $routes = array(
     'report-costs' => 'Просмотр стоимостей',
+    'report-costs-active' => 'Просмотр текущих стоимостей',
     'report-managers' => 'Зарплата менеджеров',
     'report-workers' => 'Фонд ЗП цеха',
     'report-clients' => 'Клиенты',
