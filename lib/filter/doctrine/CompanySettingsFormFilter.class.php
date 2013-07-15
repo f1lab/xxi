24df50f5c78dab32bf214b57a073679a00f6c772
Anatoly Pashin <anatoly.pashin@gmail.com>
2013-07-15 17:41:05 +1100

    removed typo model and generated all base classes

---
+++ lib/filter/doctrine/CompanySettingsFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CompanySettings filter form.
+ *
+ * @package    xxi
+ * @subpackage filter
+ * @author     Saritskiy Roman
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CompanySettingsFormFilter extends BaseCompanySettingsFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/doctrine/ShareSettingsFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * ShareSettings filter form.
+ *
+ * @package    xxi
+ * @subpackage filter
+ * @author     Saritskiy Roman
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ShareSettingsFormFilter extends BaseShareSettingsFormFilter
+{
+  public function configure()
+  {
+  }
+}
