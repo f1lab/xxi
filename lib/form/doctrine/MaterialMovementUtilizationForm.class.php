5e611c7b50545a32cd2dba2897a03998a55b4e8e
Anatoly Pashin <anatoly.pashin@gmail.com>
2014-04-08 13:08:23 +1100

    MaterialMovementUtilization model

---
+++ lib/form/doctrine/MaterialMovementUtilizationForm.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * MaterialMovementUtilization form.
+ *
+ * @package    xxi
+ * @subpackage form
+ * @author     Saritskiy Roman
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class MaterialMovementUtilizationForm extends BaseMaterialMovementUtilizationForm
+{
+  public function configure()
+  {
+    unset (
+      $this['deleted_at']
+      , $this['created_at']
+      , $this['updated_at']
+      , $this['created_by']
+      , $this['updated_by']
+    );
+  }
+}
