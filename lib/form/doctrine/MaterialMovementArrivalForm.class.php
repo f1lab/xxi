f856d7b7617cd5c09e261fbcf5e9d7f7f40fedfa
Anatoly Pashin <anatoly.pashin@gmail.com>
2014-04-08 13:11:08 +1100

    MaterialMovementArrival model

---
+++ lib/form/doctrine/MaterialMovementArrivalForm.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * MaterialMovementArrival form.
+ *
+ * @package    xxi
+ * @subpackage form
+ * @author     Saritskiy Roman
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class MaterialMovementArrivalForm extends BaseMaterialMovementArrivalForm
+{
+  public function configure()
+  {
+    unset (
+      $this['deleted_at']
+      , $this['created_at']
+      , $this['updated_at']
+      , $this['created_by']
+      , $this['updated_by']
+    );
+  }
+}
