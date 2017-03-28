72e6b520f1cf0f7ae4b13b1f234246feedfcfc33
Anatoly Pashin <anatoly.pashin@gmail.com>
2017-03-28 19:01:38 +1000

    better plan modal

--- apps/frontend/modules/plan/templates/_modal.php
+++ apps/frontend/modules/plan/templates/_modal.php
@@ -1,15 +1,27 @@
+<?php
+/**
+ * @var RefOrderWork $ref
+ */
+?>
 <div class="clearfix">
-  <h4 class="pull-left">
-    Заказ №<?php echo $ref->getOrder()->getId() ?>
-    <small><a href="<?php echo url_for('order/show?id=' . $ref->getOrder()->getId()) ?>" target="_blank">ещё подробнее!</a></small>
-  </h4>
+    <h4 class="pull-left">
+        Заказ №<?php echo $ref->getOrder()->getId() ?>
+        <small><a href="<?php echo url_for('order/show?id=' . $ref->getOrder()->getId()) ?>" target="_blank">ещё
+                подробнее!</a></small>
+    </h4>
 </div>
-<div class="well">Описание заказа: <?php echo $ref->getOrder()->getDescription() ?: '—' ?></div>
-
-<h5>Работа «<?php echo $ref->getWork()->getName() ?>» на участке «<?php echo $ref->getWork()->getArea()->getName() ?>»</h5>
 <div class="well">
-  Мастер: <?php echo $ref->getMaster() ?><br />
-  Комментарий к работе: <?php echo $ref->getComment() ?: '—' ?><br /><br />
+    <b>Срок исполнения</b>: <?php echo $ref->getOrder()->getDueDate() ?: '—' ?><br>
+    <b>Описание заказа</b>: <?php echo nl2br($ref->getOrder()->getDescription()) ?: '—' ?>
+</div>
 
-  Файлы: <?php echo $ref->getOrder()->getFiles() ?: '—' ?>
+<h5>
+    <b>Работа</b> «<?php echo $ref->getWork()->getName() ?>»
+    <b>на участке</b> «<?php echo $ref->getWork()->getArea()->getName() ?>»
+</h5>
+<div class="well">
+    <b>Мастер</b>: <?php echo $ref->getMaster() ?><br/>
+    <b>Комментарий к работе</b>: <?php echo $ref->getComment() ?: '—' ?><br/><br/>
+
+    <b>Файлы</b>: <?php echo $ref->getOrder()->getFiles() ?: '—' ?>
 </div>
