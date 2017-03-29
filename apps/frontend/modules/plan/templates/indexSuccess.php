ca1ede3303b580744d5a7f67ad27472e9b954aa8
Anatoly Pashin <anatoly.pashin@gmail.com>
2017-03-29 11:35:50 +1000

    fix plan

--- apps/frontend/modules/plan/templates/indexSuccess.php
+++ apps/frontend/modules/plan/templates/indexSuccess.php
@@ -100,37 +100,6 @@
             , filterChangeHandler = function () {
             $("#calendar").fullCalendar("refetchEvents");
         }
-<<<<<<< Updated upstream
-=======
-      }
-    }]
-
-    , firstDay: 1
-    /* , minTime: 8
-    , maxTime: 22 */
-    , header: {
-      left: "agendaDay agendaWeek month"
-      , center: "title"
-    }
-    , defaultView: "agendaWeek"
-    , timeFormat: "H:mm"
-    , axisFormat: "H:mm"
-    , allDaySlot: false
-    , monthNames: ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
-    , monthNamesShort: ["Янв.","Фев.","Март","Апр.","Май","Июнь","Июль","Авг.","Сент.","Окт.","Ноя.","Дек."]
-    , dayNames: ["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"]
-    , dayNamesShort: ["ВС","ПН","ВТ","СР","ЧТ","ПТ","СБ"]
-    , buttonText: {
-      prev: "&nbsp;&#9668;&nbsp;"
-      , next: "&nbsp;&#9658;&nbsp;"
-      , prevYear: "&nbsp;&lt;&lt;&nbsp;"
-      , nextYear: "&nbsp;&gt;&gt;&nbsp;"
-      , today: "Сегодня"
-      , month: "Месяц"
-      , week: "Неделя"
-      , day: "День"
-    }
->>>>>>> Stashed changes
 
         $("#new-events .fc-event").each(function () {
             var eventObject = {
@@ -204,7 +173,7 @@
              , maxTime: 22 */
             ,
             header: {
-                left: "agendaWeek month"
+                left: "agendaDay agendaWeek month"
                 , center: "title"
             }
             ,
